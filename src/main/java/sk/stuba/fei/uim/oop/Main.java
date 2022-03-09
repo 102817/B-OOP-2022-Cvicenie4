d8d7d7bb321ab5078045f90f99ca624ebca96529
Viktoriia Fedynaik <xfedyniak@stuba.sk>
2022-03-09 15:30:45 +0100

    ukAZKA PROTECTED

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -3,5 +3,11 @@ package sk.stuba.fei.uim.oop;
 public class Main {
     public static void main(String[] args) {
 
+        TesT test = new TesT();
+        test.tri=4;
+
+        do {
+
+        }while (test.tri ==3);
     }
 }
---
+++ src/main/java/sk/stuba/fei/uim/oop/TesT.java
@@ -0,0 +1,7 @@
+package sk.stuba.fei.uim.oop;
+
+public class TesT {
+    public int jeden;
+    private int dva;
+    protected int tri;
+}
